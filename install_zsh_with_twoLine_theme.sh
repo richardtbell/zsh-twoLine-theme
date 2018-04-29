a6d498922091e56f6049317928860dbfdfee15ae
Richard <richard.t.bell90@gmail.com>
2018-04-29 09:40:55 +0100

    Add script to install zsh and add twoLine theme

---
+++ install_zsh_with_twoLine_theme.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+curl -o ~/.oh-my-szh/themes/twoLine.zsh-theme https://raw.githubusercontent.com/richardtbell/zsh-twoLine-theme/master/twoLine.zsh-theme
+sed -i -e 's/ZSH_THEME="robbyrussell"/ZSH_THEME="twoLine"/g' ~/.zshrc
+source ~/.zshrc
+
+echo "twoLine theme added"
