7d48fb2660c26c26f6d668f2c8ff9cdd52884c35
Richard <richard.t.bell90@gmail.com>
2018-04-29 10:05:18 +0100

    Fix typo in install script

--- install_zsh_with_twoLine_theme.sh
+++ install_zsh_with_twoLine_theme.sh
@@ -2,7 +2,7 @@
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
-curl -o ~/.oh-my-szh/themes/twoLine.zsh-theme https://raw.githubusercontent.com/richardtbell/zsh-twoLine-theme/master/twoLine.zsh-theme
+curl -o ~/.oh-my-zsh/themes/twoLine.zsh-theme https://raw.githubusercontent.com/richardtbell/zsh-twoLine-theme/master/twoLine.zsh-theme 
 sed -i -e 's/ZSH_THEME="robbyrussell"/ZSH_THEME="twoLine"/g' ~/.zshrc
 source ~/.zshrc
 
